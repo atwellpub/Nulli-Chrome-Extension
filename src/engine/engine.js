1a1f03d1b5d7a407b7ee00cf82e7aa86df7a73c4
Hudson Atwell <atwell.publishing@gmail.com>
2014-10-18 19:46:10 -0500

    Second commit. All the lacks is writing /src/engine/engine.js to read profiles and load js/css.

--- src/engine/engine.js
+++ src/engine/engine.js
@@ -1,13 +1,20 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
-
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
-
-
-//example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
+/**
+ *  Load profiles and run taskss
+ */
+chrome.storage.sync.get('profiles' , function(obj) {
+	console.log("Nulli has loaded " + obj.lenngth + "profiles.");
+	console.log(obj);
+	
+	var Engine = ( function() {
+		
+		var construct = {
+		
+		
+		
+		}
+		
+		
+		return construct;
+	
+	});
+});
