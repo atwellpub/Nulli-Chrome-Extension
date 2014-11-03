d3306bbdf68d531f5862d01d2c944ad80ff0ef37
Hudson Atwell <atwell.publishing@gmail.com>
2014-11-02 19:30:57 -0600

    V1 ready

--- src/engine/engine.js
+++ src/engine/engine.js
@@ -1,20 +1,69 @@
+
 /**
- *  Load profiles and run taskss
+ *	Load profiles and run tasks
  */
-chrome.storage.sync.get('profiles' , function(obj) {
-	console.log("Nulli has loaded " + obj.lenngth + "profiles.");
+ 
+
+chrome.storage.sync.get( 'profiles' , function(obj) {
+
+	console.log("Nulli has loaded profiles.");
 	console.log(obj);
-	
 	var Engine = ( function() {
-		
+
+		var siteuri = window.location.href;
+
 		var construct = {
-		
-		
-		
+
+			init: function() {
+
+				for( var id in obj.profiles ) {
+
+					if ( obj.profiles[id]['profile-toggle'] == 'off' ) {
+						continue;
+					}
+
+
+					if ( siteuri.indexOf( obj.profiles[id]['search-condition'] ) == -1 ) {
+						continue;
+					}
+
+					
+					/* Evaluate Javascript */
+					if ( typeof obj.profiles[id]['javascript'] != 'undefined' ) {
+						eval(obj.profiles[id]['javascript']);
+					}
+
+					/* Adds CSS */
+					if ( typeof obj.profiles[id]['css'] != 'undefined' ) {
+						var body = document.getElementById('body');
+						body.insertAdjacentHTML('beforeend', '<style type="text/css">'+obj.profiles[id]['css']+'</style>');
+						
+					}
+					
+					/*  Google Analytics add rule processed event  					 
+					(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+					(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+					m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+					})(window,document,'script','https://www.google-analytics.com/analytics.js','ga'); // Note: https protocol here
+					 
+					ga('create', 'UA-56260507-2', 'auto');
+					ga('set', 'checkProtocolTask', function(){}); // Removes failing protocol check. @see: http://stackoverflow.com/a/22152353/1958200
+					ga('require', 'displayfeatures');
+					ga('send', 'event', 'Rule Applied', 'Rule Applied' , 1 );*/
+					
+				}
+			}
+
 		}
-		
-		
+
+
 		return construct;
+
+	})();
+
 	
-	});
+	/* Load Engine */
+	Engine.init();
+
+	
 });
